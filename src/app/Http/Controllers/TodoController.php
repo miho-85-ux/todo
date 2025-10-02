a49498380d876036503381a04cf9b10aa65331b4
yokoyama miho <mmmiho0805mm@hotmail.co.jp>
2025-10-03 00:16:16 +0900

    バリデーション機能の追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TodoRequest;
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
     public function index() {
-        return view('index');
+        $todos = Todo::all();
+        return view('index', compact('todos'));
+    }
+
+    public function store(TodoRequest $request) {
+        $todo = $request->only(['content']);
+        Todo::create($todo);
+
+        return redirect('/')->with('message', 'Todoを作成しました');
     }
 }
--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,4 +1,4 @@
-d@extends('layouts.app')
+@extends('layouts.app')
 
 @section('css')
 <link rel="stylesheet" href="{{ asset('css/index.css') }}">
@@ -7,13 +7,25 @@ d@extends('layouts.app')
 @section('content')
 
 <div class="todo__alert">
+    @if (session('message'))
     <div class="todo__alert--success">
-        Todoを作成しました
+        {{ session('message') }}
     </div>
+    @endif
+    @if ($errors->any())
+    <div class="todo__alert--danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
 </div>
 
 <div class="todo__content">
-    <form class="create-form">
+    <form class="create-form" action="/todos" method="post">
+        @csrf 
         <div class="create-form__item">
             <input class="create-form__item-input" type="text" name="content">
         </div>
@@ -26,11 +38,12 @@ d@extends('layouts.app')
             <tr class="todo-table__row">
                 <th class="todo-table__header">Todo</th>
             </tr>
+            @foreach ($todos as $todo)
             <tr class="todo-table__row">
                 <td class="todo-table__item">
                     <form class="update-form">
                         <div class="update-form__item">
-                            <input class="update-form__item-input" type="text" name="content" value="test">
+                            <input class="update-form__item-input">{{ $todo['content'] }}</input>
                         </div>
                         <div class="update-form__button">
                             <button class="update-form__button-submit" type="submit">更新</button>
@@ -45,25 +58,7 @@ d@extends('layouts.app')
                     </form>
                 </td>
             </tr>
-            <tr class="todo-table__row">
-                <td class="todo-table__item">
-                    <form class="update-form">
-                        <div class="update-form__item">
-                            <input class="update-form__item-input" type="text" value="test2">
-                        </div>
-                        <div class="update-form__button">
-                            <button class="update-form__button-submit" type="submit">更新</button>
-                        </div>
-                    </form>
-                </td>
-                <td class="todo-table__item">
-                    <form class="delete-form">
-                        <div class="delete-form__button">
-                            <button class="delete-form__button-submit" type="submit">削除</button>
-                        </div>
-                    </form>
-                </td>
-            </tr>
+            @endforeach
         </table>
     </div>
 </div>
