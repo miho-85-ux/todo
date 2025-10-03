f1185614961720bf5ffaedef8b2b1a23fc52d032
yokoyama miho <mmmiho0805mm@hotmail.co.jp>
2025-10-03 17:47:12 +0900

    更新・削除機能追加

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -41,9 +41,12 @@
             @foreach ($todos as $todo)
             <tr class="todo-table__row">
                 <td class="todo-table__item">
-                    <form class="update-form">
+                    <form class="update-form" action="/todos/update" method="POST">
+                        @method('patch')
+                        @csrf 
                         <div class="update-form__item">
-                            <input class="update-form__item-input">{{ $todo['content'] }}</input>
+                            <input class="update-form__item-input" type="text" name="content" value="{{ $todo['content'] }}">
+                            <input type="hidden" name="id" value="{{ $todo['id'] }}">
                         </div>
                         <div class="update-form__button">
                             <button class="update-form__button-submit" type="submit">更新</button>
@@ -51,8 +54,11 @@
                     </form>
                 </td>
                 <td class="todo-table__item">
-                    <form class="delete-form">
+                    <form class="delete-form" action="/todos/delete" method="POST">
+                        @method('delete')
+                        @csrf 
                         <div class="delete-form__button">
+                            <input type="hidden" name="id" value="{{ $todo['id'] }}">
                             <button class="delete-form__button-submit" type="submit">削除</button>
                         </div>
                     </form>
